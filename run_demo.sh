10b7d392250136b63057147459938cfad47566ee
Aviv Laufer <aviv.laufer@rokt.com>
2024-08-26 10:40:29 -0400

    Run demo app

---
+++ run_demo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This script is used to run the demo application.
+# It starts the protoconf agent, which is responsible for managing the configuration.
+# Then it starts the demo application, which uses the protoconf agent to dynamically update its configuration.
+# Finally, it stops the protoconf agent after the demo application has finished running.
+
+pkill -9 protoconf >/dev/null 2>&1
+protoconf agent -dev tests/test_data/. &
+python demo_app.py    
+pkill  -9 protoconf >/dev/null 2>&1
+
