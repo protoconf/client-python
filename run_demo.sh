d291f05c44ad80acf72fbe80874daec8c0954a42
Aviv Laufer <aviv.laufer@rokt.com>
2024-08-26 13:08:13 -0400

    Update PYTHONPATH

--- run_demo.sh
+++ run_demo.sh
@@ -3,7 +3,7 @@
 # It starts the protoconf agent, which is responsible for managing the configuration.
 # Then it starts the demo application, which uses the protoconf agent to dynamically update its configuration.
 # Finally, it stops the protoconf agent after the demo application has finished running.
-
+export PYTHONPATH=$PYTHONPATH:$(pwd)/tests
 pkill -9 protoconf >/dev/null 2>&1
 protoconf agent -dev tests/test_data/. &
 python demo_app/demo_app.py
--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,10 @@
 # This script is designed to run tests for the protoconfloader project.
 # It starts the protoconf agent in development mode, runs pytest with coverage,
 # and then stops the protoconf agent.
-
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 pkill -9 protoconf >/dev/null 2>&1
 protoconf agent -dev tests/test_data/. &
-pytest --cov=protoconfloader tests/ 
+#pytest --cov=protoconfloader tests/ 
+python -m pytest -s  
 pkill  -9 protoconf >/dev/null 2>&1
 
